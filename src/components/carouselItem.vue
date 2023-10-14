91bad66fa0e1e46b44b79cf413fc7849eb4e9be6
Alfie213 <arturbogoratov@gmail.com>
2023-10-14 19:31:53 +0300

    Test blocks

--- src/components/carouselItem.vue
+++ src/components/carouselItem.vue
@@ -37,9 +37,37 @@ function init(){
             </div>
         </div>
     </div>
+    <div class="blocks-container">
+      <div v-for="(item, index) in items" :key="index" class="my-block">
+        <p>Блок {{ index + 1 }}</p>
+        <p>{{ item }}</p>
+      </div>
+    </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      items: ["Первый блок", "Второй блок", "Третий блок"]
+    };
+  }
+};
+</script>
+
 <style>
+    .blocks-container {
+    text-align: center; /* Центрировать блоки по горизонтали */
+    }
+
+    .my-block {
+    display: inline-block; /* Выстраиваем блоки в ряд */
+    margin: 10px; /* Добавляем отступ между блоками */
+    padding: 10px;
+    border: 1px solid #ccc;
+    background-color: lightblue;
+    }
+
     .item {
         display: flex;
         flex-direction: column;
